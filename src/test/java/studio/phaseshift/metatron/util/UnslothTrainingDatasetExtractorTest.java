68010665828bdf162136ca2bc95ac051ccf257e3
Marko A. Rodriguez <okrammarko@gmail.com>
2026-08-02 23:19:06 -0600

    disabled UnslothTrainingDatasetExtractor as it just creates git noise. Only need to run it during training sessions.

--- src/test/java/studio/phaseshift/metatron/util/UnslothTrainingDatasetExtractorTest.java
+++ src/test/java/studio/phaseshift/metatron/util/UnslothTrainingDatasetExtractorTest.java
@@ -1,8 +1,10 @@
 package studio.phaseshift.metatron.util;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import studio.phaseshift.metatron.AbstractMetatronTest;
 
+@Disabled
 public class UnslothTrainingDatasetExtractorTest extends AbstractMetatronTest {
     @Test
     public void runExtractor() {
