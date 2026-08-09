11286cb87b61f63df58f996755cfb3e7f99cd7a3
Marko A. Rodriguez <okrammarko@gmail.com>
2026-08-08 18:01:25 -0600

    added a skip test for web_httpHandler if the web server can't be bound to port.

--- src/test/java/studio/phaseshift/metatron/isa/web/space/http/web_httpHandlerTest.java
+++ src/test/java/studio/phaseshift/metatron/isa/web/space/http/web_httpHandlerTest.java
@@ -1,12 +1,12 @@
 /*
  * metatron: a distributed virtual machine and language
  *  Copyright (C) 2025- PhaseShift Studio, LLC
- *  
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *  
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -21,6 +21,7 @@ package studio.phaseshift.metatron.isa.web.space.http;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import studio.phaseshift.metatron.SkipWhenPortUnavailable;
 import studio.phaseshift.metatron.furi.fURI;
 import studio.phaseshift.metatron.isa.Space;
 import studio.phaseshift.metatron.isa.m.space.memSpace;
@@ -41,6 +42,7 @@ import static studio.phaseshift.metatron.isa.m.type.impl.MUri.uri;
 /*
  * @author Marko A. Rodriguez (http://markorodriguez.com)
  */
+@SkipWhenPortUnavailable(value = 80)
 public class web_httpHandlerTest extends AbstractHTTPServerTest {
 
     private Space contentSpace;
